25b342df9af6652c2c31dab8834029f61fc50b6d
Rdbo <57117082+rdbo@users.noreply.github.com>
2021-10-10 15:50:52 -0300

    Add files via upload

--- autoclicker.py
+++ autoclicker.py
@@ -1,25 +1,46 @@
 from pynput.mouse import Button, Listener, Controller
-import threading
 import time
 import random
+import argparse
 
 mouse = Controller()
-btn_click = Button.left
-btn_listen = Button.button8
-is_down = False
-# pause = 0.01 # Minimum mouse click delay (unused)
-cps_range = [i for i in range(10, 14)] # From 10 to 14 CPS
+buttons = {
+	"left" : Button.left,
+	"right" : Button.right,
+	"middle" : Button.middle,
+	"x1" : Button.button8,
+	"x2" : Button.button9
+}
+btn_listen = None
+btn_click = None
+cps_delays = []
+is_pressed = False
 
 def on_click(x, y, button, pressed):
-	global is_down
+	global is_pressed
 	if button == btn_listen:
-		print(f"State: {pressed}")
-		is_down = pressed
+		print(f"{button} : {pressed}")
+		is_pressed = pressed
 
-listener = Listener(on_click=on_click)
-listener.start()
 
-while True:
-	if is_down:
-		mouse.click(btn_click)
-		time.sleep(1 / random.choice(cps_range))
+if __name__ == "__main__":
+	btn_options = [btn for btn in buttons]
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-l", "--listen", action="store", dest="listen", default=btn_options[-1], choices=btn_options, required=False)
+	parser.add_argument("-c", "--click", action="store", dest="click", default=btn_options[0], choices=btn_options, required=False)
+	parser.add_argument("-m", "--min", action="store", dest="min", default=8, type=int, required=False)
+	parser.add_argument("-x", "--max", action="store", dest="max", default=16, type=int, required=False)
+	args = parser.parse_args()
+	if args.min > args.max:
+		parser.print_help()
+		exit(0)
+	btn_listen = buttons[args.listen]
+	btn_click = buttons[args.click]
+	cps_delays = [(1 / i) for i in range(args.min, args.max + 1)]
+	listener = Listener(on_click=on_click)
+	listener.start()
+
+	while True:
+		if is_pressed:
+			mouse.click(btn_click)
+			time.sleep(random.choice(cps_delays))
